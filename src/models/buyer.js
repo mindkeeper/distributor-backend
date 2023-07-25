a83bc193f4fddf195f5dd34f83135c60f15a42b7
mindkeeper <fcb.nyak@gmail.com>
2023-07-25 19:23:14 +0700

    adding distributor to buyer model

--- src/models/buyer.js
+++ src/models/buyer.js
@@ -7,10 +7,11 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate({ Area, Transaction }) {
+    static associate({ Area, Transaction, Distributor }) {
       // define association here
       this.belongsTo(Area, { foreignKey: "area_id" });
       this.hasMany(Transaction, { foreignKey: "buyer_id" });
+      this.belongsTo(Distributor, { foreignKey: "distributor_id" });
     }
   }
   Buyer.init(
@@ -24,6 +25,15 @@ module.exports = (sequelize, DataTypes) => {
         },
       },
       address: DataTypes.STRING,
+      phone: {
+        type: DataTypes.STRING,
+        validate: {
+          is: {
+            args: /^(\+62|62)?[\s-]?0?8[1-9]{1}\d{1}[\s-]?\d{4}[\s-]?\d{2,5}$/,
+            msg: "invalid phone number format",
+          },
+        },
+      },
     },
     {
       sequelize,
