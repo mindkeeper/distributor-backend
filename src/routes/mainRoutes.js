43d7900b393f9278883548b70a58e11c92e2d26c
mindkeeper <fcb.nyak@gmail.com>
2023-07-26 20:33:28 +0700

    addinsg distributors routes

--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -1,9 +1,11 @@
 const routes = require("express").Router();
 const authRoutes = require("./auth");
 const userRoutes = require("./user");
+const distibutorRoutes = require("./distributor");
 const prefix = "/api/v1";
 
 routes.use(`${prefix}/auth`, authRoutes);
 routes.use(`${prefix}/user`, userRoutes);
+routes.use(`${prefix}/distributor`, distibutorRoutes);
 
 module.exports = routes;
