c250429a88400e305c11d2eee1fbe2bca4ef145a
mindkeeper <fcb.nyak@gmail.com>
2023-07-26 20:32:35 +0700

    updating check password to return Custom Error

--- src/utils/checkPassword.js
+++ src/utils/checkPassword.js
@@ -1,10 +1,13 @@
+const CustomError = require("./CustomError");
+
 module.exports = function checkPassword(password) {
   if (password.length < 6 || password > 20)
-    throw new Error("password must be 6-20 character long");
+    throw new CustomError("password must be 6-20 character long", 400);
   const passRegex = /^(?=.*[A-Za-z])(?=.*\d)/gm;
   if (!passRegex.test(password)) {
-    throw new Error(
-      "password must contain at least one character and one number"
+    throw new CustomError(
+      "password must contain at least one character and one number",
+      400
     );
   }
 };
