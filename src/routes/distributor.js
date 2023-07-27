7537e2dd68f3d0b5b9c2b016f2bb9fd89bba58d2
mindkeeper <fcb.nyak@gmail.com>
2023-07-27 15:49:19 +0700

    adding create employees endpoint

--- src/routes/distributor.js
+++ src/routes/distributor.js
@@ -1,9 +1,14 @@
 const createDistributorHandler = require("../controllers/distributors/createDistributor");
+const createTokenHandler = require("../controllers/distributors/createToken");
 const getDistributorDetail = require("../controllers/distributors/getDetail");
 const verifyToken = require("../middlewares/authorization/verifyToken");
-
+const verifyDistributorToken = require("../middlewares/authorization/verifyDistributorToken");
+const addEmployeesHandler = require("../controllers/distributors/addEmployees");
 const routes = require("express").Router();
+
 routes.post("/new", verifyToken, createDistributorHandler);
 routes.get("/id/:distributorId", verifyToken, getDistributorDetail);
+routes.post("/create-token/:distributorId", verifyToken, createTokenHandler);
+routes.post("/employees/new", verifyDistributorToken, addEmployeesHandler);
 
 module.exports = routes;
