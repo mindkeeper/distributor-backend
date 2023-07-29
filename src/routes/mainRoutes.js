4e511270701a54f862c8a33b4c453be30f3ff3ac
mindkeeper <fcb.nyak@gmail.com>
2023-07-29 19:08:46 +0700

    adding company routes

--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -2,11 +2,12 @@ const routes = require("express").Router();
 const authRoutes = require("./auth");
 const userRoutes = require("./user");
 const distributorRoutes = require("./distributor");
-
+const companyRoutes = require("./company");
 const prefix = "/api/v1";
 
 routes.use(`${prefix}/auth`, authRoutes);
 routes.use(`${prefix}/user`, userRoutes);
 routes.use(`${prefix}/distributor`, distributorRoutes);
+routes.use(`${prefix}/company`, companyRoutes);
 
 module.exports = routes;
