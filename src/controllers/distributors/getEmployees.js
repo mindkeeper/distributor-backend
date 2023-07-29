6178193529d58f898fc230966b6aaeb00f3d8d4b
mindkeeper <fcb.nyak@gmail.com>
2023-07-29 19:06:24 +0700

    adding distributor_id conditions when searching

--- src/controllers/distributors/getEmployees.js
+++ src/controllers/distributors/getEmployees.js
@@ -5,19 +5,22 @@ const getPagination = require("../../utils/getPagination");
 const getEmployeesHandler = asyncErrorHandler(async (req, res, next) => {
   const { name, role, page = 1, limit = 5 } = req.query;
   const offset = (parseInt(page) - 1) * parseInt(limit);
+  const { distributor_id } = req.distributorPayload;
   const { Op } = Sequelize;
   const conditions = {
     ...(!name ? {} : { name: { [Op.iLike]: `%${name.toLowerCase()}%` } }),
     ...(!role ? {} : { role }),
+    distributor_id,
   };
   const { count, rows: employees } = await DistributorUser.findAndCountAll({
     where: conditions,
     offset,
     limit: parseInt(limit),
+    attributes: ["id", "name", "role"],
   });
   if (employees.length === 0)
     return next(new CustomError("Data pegawai tidak ditemukan", 404));
-  const pagination = getPagination(count, req);
+  const pagination = getPagination(count, page, limit);
   return res
     .status(200)
     .json({ message: "success", data: { employees, pagination } });
