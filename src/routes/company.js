740aa6be1de6c1e81e985e27705be00a371694b1
mindkeeper <fcb.nyak@gmail.com>
2023-07-29 23:39:13 +0700

    adding get by id and edit company endpoint

--- src/routes/company.js
+++ src/routes/company.js
@@ -1,9 +1,13 @@
 const routes = require("express").Router();
 const createCompanyHandler = require("../controllers/companies/createCompany");
+const editCompanyHandler = require("../controllers/companies/editCompany");
+const getByIdHandler = require("../controllers/companies/getById");
 const getCompaniesHandler = require("../controllers/companies/getCompanies");
 const verifyDistributorToken = require("../middlewares/authorization/verifyDistributorToken");
 
 routes.post("/new", verifyDistributorToken, createCompanyHandler);
 routes.get("/all", verifyDistributorToken, getCompaniesHandler);
+routes.get("/:id", verifyDistributorToken, getByIdHandler);
+routes.patch("/edit/:id", verifyDistributorToken, editCompanyHandler);
 
 module.exports = routes;
