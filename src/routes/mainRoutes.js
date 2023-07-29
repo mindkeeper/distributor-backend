4957a2da09057d7adbd5c9862a910f7562b19a7c
mindkeeper <fcb.nyak@gmail.com>
2023-07-29 19:08:12 +0700

    typo in naming distributor routes

--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -1,11 +1,12 @@
 const routes = require("express").Router();
 const authRoutes = require("./auth");
 const userRoutes = require("./user");
-const distibutorRoutes = require("./distributor");
+const distributorRoutes = require("./distributor");
+
 const prefix = "/api/v1";
 
 routes.use(`${prefix}/auth`, authRoutes);
 routes.use(`${prefix}/user`, userRoutes);
-routes.use(`${prefix}/distributor`, distibutorRoutes);
+routes.use(`${prefix}/distributor`, distributorRoutes);
 
 module.exports = routes;
